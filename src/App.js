5eef54dffa8314630de295c7e5082e1fcec50d2c
ramblelob <aileen.ramelb@gmail.com>
2022-01-15 07:49:03 -0800

    Created a to-do list app with components folder

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import './App.css';
+import Form from './components/Form';
+import TodoList from './components/TodoList';
+
+function App() {
+  const[inputText, setInputText] = useState("");
+  const[todos, setTodos] = useState([]);
+  return (
+    <div className="App">
+      <header>
+        <h1>Darshini's Todo List</h1>
+      </header>
+      <Form setInputText={setInputText} />
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
