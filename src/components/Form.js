6d1d88f68533f3f2e4c951a2ce64178098815251
ramblelob <aileen.ramelb@gmail.com>
2022-01-15 08:35:22 -0800

    Updated form to store textInput value, rendering state  after submission

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,15 +1,28 @@
 import React from 'react';
+import TodoList from './TodoList';
 
-function Form({setInputText}) {
+function Form({ setInputText, todos, setTodos, inputText}) {
 const inputTextHandler = (e) => {
   console.log(e.target.value);
   setInputText(e.target.value);
 };
 
+const submitToDoHandler = (e) => {
+  e.preventDefault();
+  setTodos([
+    ...todos,{text: inputText, completed: false, id: Math.random() * 1000}
+  ]);
+  setInputText("");
+};
+
     return (
       <form>
-          <input onClick={inputTextHandler} type="text" className="todo-input" />
-          <button className="todo-button" type="submit">
+          <input 
+          value={inputText}
+          onChange={inputTextHandler} 
+          type="text" 
+          className="todo-input" />
+          <button onClick={submitToDoHandler} className="todo-button" type="submit">
             <i className="fa-regular fa-square-plus"></i>
           </button>
           <div className="select">
