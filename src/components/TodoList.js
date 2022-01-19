deb33a591b7cbba3bc56b3d2271df8b52771cc4a
ramblelob <aileen.ramelb@gmail.com>
2022-01-19 12:22:03 -0800

    Created TodoList component  in filtering todos

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,9 +1,25 @@
 import React from "react";
+import Todo from "./Todo";
 
-function TodoList() {
+
+
+function TodoList({todos, setTodos, filteredTodos}) {
+ 
     return (
         <div className="todo-container">
-            <ul className="todo-list"></ul>
+            <ul className="todo-list">
+                {filteredTodos.map((todo) => (
+                    <Todo 
+                    setTodos={setTodos} 
+                    todos={todos} 
+                    key={todo.id}
+                    todo={todo} 
+                    text={todo.text}
+                    
+                     />
+                ))}
+            </ul>
+           
         </div>
     );
   }
