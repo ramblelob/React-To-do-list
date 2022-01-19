364128ce8e78f1efa64bf048737d5ee5211f7c52
ramblelob <aileen.ramelb@gmail.com>
2022-01-19 12:21:12 -0800

    Created Form component for filtering input text

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import TodoList from './TodoList';
 
-function Form({ setInputText, todos, setTodos, inputText}) {
+function Form({ setInputText, todos, setTodos, inputText, setStatus}) {
 const inputTextHandler = (e) => {
-  console.log(e.target.value);
   setInputText(e.target.value);
 };
 
@@ -15,6 +14,10 @@ const submitToDoHandler = (e) => {
   setInputText("");
 };
 
+  const statusHandler = (e) => {
+    setStatus(e.target.value);  
+  }
+
     return (
       <form>
           <input 
@@ -23,10 +26,10 @@ const submitToDoHandler = (e) => {
           type="text" 
           className="todo-input" />
           <button onClick={submitToDoHandler} className="todo-button" type="submit">
-            <i className="fa-regular fa-square-plus"></i>
+            <i className="fas fa-plus-square"></i>
           </button>
           <div className="select">
-              <select name="todos" className="filter-todo">
+              <select onChange={statusHandler} name="todos" className="filter-todo">
                   <option value="all">All</option>
                   <option value="completed">Completed</option>
                   <option value="uncompleted">Uncompleted</option>
